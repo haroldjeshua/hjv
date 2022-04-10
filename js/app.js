2353376214ff93ed6f434370c1e903cc8e85807b
Harold Jeshua Varde <harvardii15@gmail.com>
2022-04-11 02:12:29 +0800

    added active class func + topbar

--- js/app.js
+++ js/app.js
@@ -1,8 +1,31 @@
+// Topbar
+const topBar = {
+	classname: 'topbar'
+};
+
+let nanobar = new Nanobar( topBar );
+nanobar.go(100);
+
+
+// Navbar
+const navbar = document.querySelector('.navbar'),
+	navbarItems = navbar.querySelectorAll('.navbar__link');
+
+navbar.onclick = function(e) {
+    console.log("sip")
+    for (let i = 0; i < navbarItems.length; i++) {
+        navbarItems[i].classList.remove('active');
+    }
+    e.target.classList.add('active');
+}
+
+
+// AOS
 AOS.init( {
-    offset: 200,
-    duration: 600,
-    easing: 'ease-in-sine',
-    delay: 200,
-    once: true,
-    disable: 'mobile'
+	offset: 200,
+	duration: 600,
+	easing: 'ease-in-sine',
+	delay: 200,
+	once: true,
+	disable: 'mobile'
 });

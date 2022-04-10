2b01ba57bbf50c525d74a0830b62ca43b108838e
Harold Jeshua Varde <harvardii15@gmail.com>
2022-04-11 03:02:21 +0800

    updated page activation

--- js/app.js
+++ js/app.js
@@ -9,18 +9,40 @@ nanobar.go(66);
 nanobar.go(100);
 
 
-// Navbar
+// Navbar + page activate
 const navbar = document.querySelector('.navbar'),
-	navbarItems = navbar.querySelectorAll('.navbar__link');
+	navbarItems = navbar.querySelectorAll('.navbar__link'),
+	pages = document.querySelectorAll('.page')
 
-navbar.onclick = function(e) {
-    console.log("sip")
-    for (let i = 0; i < navbarItems.length; i++) {
-        navbarItems[i].classList.remove('active');
-    }
-    e.target.classList.add('active');
+// navbar.onclick = function(e) {
+//     console.log("sip")
+//     for (let i = 0; i < navbarItems.length; i++) {
+//         navbarItems[i].classList.remove('active');
+//     }
+//     e.target.classList.add('active');
+// }
+
+function activatePage(e) {
+	e.preventDefault();
+
+	navbarItems.forEach(function(item, index) {
+		item.classList.remove('active');
+	});
+
+	[].forEach.call(pages, function(pane, index) {
+		pane.classList.remove('active');
+	})
+
+	e.target.classList.add('active');
+
+	let clickedPage = e.target.getAttribute('href');
+
+	document.querySelector(clickedPage).classList.add('active');
 }
 
+navbarItems.forEach(function(item, index) {
+	item.addEventListener('click', activatePage)
+})
 
 // AOS
 AOS.init( {
